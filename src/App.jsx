4d65ab99bc9d728fb7d521532628c5581f04d658
Vasilchenk23 <vasilchenko.maksim374@gmail.com>
2024-04-13 20:09:55 +0300

    create add new sea-animal component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import Index from "../src/pages/Home/Index"
+import SeaAnimal from "./pages/AddSeaAnimal/SeaAnimal";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './style.css'
 
 function App() {
 
   return (
     <>
-      <Index/>
+      <Router>
+      <Routes>
+        <Route path="/" element={<Index/>} />
+        <Route path="/add" element={<SeaAnimal/>} />
+      </Routes>
+    </Router>
     </>
   )
 }
